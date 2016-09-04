0cdede18e02535736f6c613e5b491f80d26d27bc
kriswithank <kristopherswann@gmail.com>
2016-09-04 14:55:43 -0500

    Fix issue with book_index url

--- coursenotes/urls.py
+++ coursenotes/urls.py
@@ -5,6 +5,6 @@ app_name = 'coursenotes'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^book/([0-9]+/$)', views.book_index, name="book_index"),
+    url(r'^book/([0-9]+)/$', views.book_index, name="book_index"),
     url(r'^book/([0-9]+)/ch/([0-9]+)/$', views.chapter_view, name="chapter_view"),
 ]
